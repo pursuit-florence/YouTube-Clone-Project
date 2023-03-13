8efbb2dc83a8ffa38edd11d5f04daf5b46e7f44f
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-03-13 11:33:36 -0400

    home added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Home from "./component/common/Home"
+import {getVideos} from "./api/fetch.js"
+// import './App.css';
+
+
 
 function App() {
+const [allVideos, setAllVideos] = useState([])
+  
+useEffect(() => {
+    getVideos().then(response => {
+      setAllVideos(response)
+    }).catch((error) => console.log(error))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home allVideos={allVideos}/>} />
+       
+      </Routes>
+     </Router>
     </div>
   );
 }
--- src/component/common/Home.js
+++ src/component/common/Home.js
@@ -1,3 +1,41 @@
-export default function Home() {
-    return;
+import { useState } from "react";
+import { searchedVideos } from "../../api/fetch";
+
+export default function Home({ allVideos }) {
+  const [searchString, setSearchString] = useState("")
+  const [videosArtist, setVideosArtist] = useState([])
+
+  function handleTextChange(event){ 
+    setSearchString(event.target.value)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    
+      searchedVideos(searchString).then(response => {
+    
+        setVideosArtist(response.items)
+        console.log(videosArtist)
+      }).catch((error) => console.log(error))
+    
+  }
+
+
+    return (
+      <div>
+        {/* <h2>{videosArtist.snippet}</h2> */}
+        <form onSubmit={handleSubmit}> 
+        <input
+          type="text"
+          value={searchString}
+          id="searchString"
+          onChange={handleTextChange}
+        
+        />
+    
+      <input type="submit" />
+        </form>
+      </div>
+        
+    )
   }
