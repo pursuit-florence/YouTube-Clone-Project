f3030bbf2776bb72685041df961169ad36b8c06b
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-03-16 11:13:01 -0400

    attempt on modal

--- src/component/common/Home.js
+++ src/component/common/Home.js
@@ -5,6 +5,13 @@ const key = process.env.REACT_APP_API_KEY;
 export default function Home() {
   const [searchString, setSearchString] = useState("")
   const [allVideos, setAllVideos] = useState([])
+  const [modal, setModal] = useState(true)
+
+  if(modal) {
+    document.body.classList.add('limited')
+  } else {
+    document.body.classList.remove('limited')
+  }
 
   function handleTextChange(event){ 
     setSearchString(event.target.value)
@@ -12,14 +19,12 @@ export default function Home() {
 
     function handleSubmit(event){
       event.preventDefault();
-      console.log(searchString)
-      console.log(key)
       return fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${key}&q=${searchString}&part=snippet&maxResults=3&type=video`)
       .then((results) => results.json())
       .then(response => {
       setAllVideos(response.items)
-      
-    })
+      setModal(false)
+    }).catch((error) => {setModal(true)})
 }  
    return (
       <div>
@@ -33,8 +38,15 @@ export default function Home() {
           />
           <input type="submit" />
         </form>
+        {modal ? (<div className="modal">
+            <div className="modalContent">
+                <p>Please submit a search above</p>
+                <button onClick={()=> setModal(!modal)}>close</button>
+            </div>
+        </div>) : null }
         <div className="video-listing"> 
-          {searchString.length ? <VideoIndex allVideos={allVideos}/> : <p>No Search Results Yet! Please submit search above!</p>}  
+          {searchString.length ? <VideoIndex allVideos={allVideos}/> : 
+          <p>No search result yet. Please submit a search above.</p>}  
       </div>
     </div>   
     )
